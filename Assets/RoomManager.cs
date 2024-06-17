9267f55c037a4089e2f195c9e2856f5ca9652b5e
SUMAN SINGHA <sumansingha047@gmail.com>
2024-06-17 14:34:45 +0530

    Initial Commit
    Try to make a multiplayer game using Photon PUN2

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message: "Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log(message: "Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+}
