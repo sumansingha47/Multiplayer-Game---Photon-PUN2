9f3dd3a1be4c8ca6724e40f27f4ac8566752c402
SUMAN SINGHA <sumansingha047@gmail.com>
2024-06-28 22:36:49 +0530

    Added nickname showing and spawn randomly

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
 
 public class PlayerSetup : MonoBehaviour
 {
@@ -8,9 +10,21 @@ public class PlayerSetup : MonoBehaviour
 
     public new GameObject camera;
 
+    public string nickname;
+
+    public TextMeshPro nicknameText;
+
     public void IsLocalPlayer()
     {
         movement.enabled = true;
         camera.SetActive(true);
+    }
+
+    [PunRPC]
+    public void SetNickname(string _name)
+    {
+        nickname = _name;
+
+        nicknameText.text = nickname;
     }
 }
--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -10,22 +10,41 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public GameObject player;
 
     [Space]
-    public Transform spawnPoint;
+    public Transform[] spawnPoints;
 
     [Space]
     public GameObject roomCam;
+
+    [Space]
+    public GameObject nameUI;
+    public GameObject connectingUI;
+
+    private string nickname = "unnamed";
 
     void Awake()
     {
         instance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeNickname(string _name)
+    {
+        nickname = _name;
+    }
+
+    public void JoinRoomButtonPressed()
     {
         Debug.Log(message: "Connecting...");
 
         PhotonNetwork.ConnectUsingSettings();
+
+        nameUI.SetActive(false);
+        connectingUI.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
     }
 
     public override void OnConnectedToMaster()
@@ -59,8 +78,12 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     public void SpawnPlayer()
     {
+        Transform spawnPoint = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Length)];
+
         GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
         _player.GetComponent<PlayerSetup>().IsLocalPlayer();
         _player.GetComponent<Health>().isLocalPlayer = true;
+
+        _player.GetComponent<PhotonView>().RPC("SetNickname", RpcTarget.AllBuffered, nickname);
     }
 }
