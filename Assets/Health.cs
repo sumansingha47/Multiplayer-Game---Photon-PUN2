c9a9d8ecb552a45daaf8c898c9b56a690fd10c54
SUMAN SINGHA <sumansingha047@gmail.com>
2024-07-05 13:48:06 +0530

    Adding a simple health bar

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int health;
     public bool isLocalPlayer;
 
+    public RectTransform healthBar;
+    private float originalHealthBarSize;
+
     [Header("UI")]
-    public TextMeshProUGUI healthText; 
+    public TextMeshProUGUI healthText;
+
+    private void Start()
+    {
+        originalHealthBarSize = healthBar.sizeDelta.x;
+    }
+
+    private void Update()
+    {
+        //healthBar.sizeDelta = new Vector2(originalHealthBarSize * health / 100f, healthBar.sizeDelta.y);
+    }
 
     [PunRPC]
     public void TakeDamage(int _damage)
     {
         health -= _damage;
+
+        healthBar.sizeDelta = new Vector2(originalHealthBarSize * health / 100f, healthBar.sizeDelta.y);
 
         healthText.text = health.ToString();
 
